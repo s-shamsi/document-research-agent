946372003d170e0e5a767019d887b2cfc5340a78
Saad Shamsi <saad.shamsi@protonmail.com>
2026-08-19 15:11:44 +0200

    FastAPI: Set-Up & Health-Check ✔️ Changes to be committed: 	modified:   app/main.py

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+# from pydantic import BaseModel
+# from app.agent import answer_question
+
+# class QuestionRequest(BaseModel):
+#     question: str
+
+# @app.post("/ask")
+# def ask(req: QuestionRequest):
+#     return answer_question(req.question)
